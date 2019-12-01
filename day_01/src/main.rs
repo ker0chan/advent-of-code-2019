711d68dc108536a35a63da56bfddd2918258fc8e
Diane <landais.diane@gmail.com>
2019-12-01 15:23:08 +0100

    Updated day_01 for second star

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // INPUT
     let masses = [74364, 146203, 128470, 91616, 115655, 134147, 53470,
     126471, 70040, 88750, 142353, 143329, 86356, 118399, 97959, 148345,
     117705, 87624, 63862, 71962, 106974, 66255, 119735, 78726, 93698,
@@ -12,11 +13,20 @@ fn main() {
     102560, 77098, 127534, 56415, 85703, 85580, 86787, 72029, 82533,
     132187, 70849, 98839];
 
+    // PART 1
     //Folding is similar to reduce in JS
     // m is a borrow, needs dereferencing! (*m)
     let sum = masses.iter().fold(0, |acc, m| acc + compute_fuel(*m));
 
-    println!("{}", sum);
+    println!("Part 1 answer: {}", sum);
+
+    // PART 2
+    let sum = masses.iter().fold(0, |acc, m| acc + compute_fuel_and_fuel_for_fuel(*m, 0));
+
+    //Use the example to make sure we didn't mess up?
+    assert_eq!(compute_fuel_and_fuel_for_fuel(1969, 0), 966);
+
+    println!("Part 2 answer: {}", sum);
 }
 
 fn compute_fuel(mass: u32) -> u32
@@ -28,4 +38,29 @@ fn compute_fuel(mass: u32) -> u32
 
     //Sweet, Rust rounds down on integer division
     mass/3 - 2
+}
+
+fn compute_fuel_and_fuel_for_fuel(mass: u32, mut total_fuel: u32) -> u32
+{
+    /* Fuel itself requires fuel just like a module - take its mass,
+     * divide by three, round down, and subtract 2. However, that fuel
+     * also requires fuel, and that fuel requires fuel, and so on.
+     */
+    
+    //How much fuel does this mass require?
+    let fuel_mass = compute_fuel(mass);
+
+    //Keep track of the total amount of fuel
+    total_fuel += fuel_mass;
+
+    let mut result = total_fuel;
+    //"Any mass that would require negative fuel should instead be treated as if it requires zero fuel"
+    // (This is our recursion end condition :3)
+    if fuel_mass >= 9 
+    {
+        //If the required fuel is heavy enough, it requires its own additional amount of fuel. 
+        result = compute_fuel_and_fuel_for_fuel(fuel_mass, total_fuel);
+    }
+
+    result
 }
