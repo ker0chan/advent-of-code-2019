1b12319dd77ac09fceb7764a081fe79003b9a043
ker0chan <landais.diane@gmail.com>
2019-12-03 22:59:11 +0100

    rustfmt'd

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,17 +1,16 @@
 fn main() {
     // INPUT
-    let masses = [74364, 146203, 128470, 91616, 115655, 134147, 53470,
-    126471, 70040, 88750, 142353, 143329, 86356, 118399, 97959, 148345,
-    117705, 87624, 63862, 71962, 106974, 66255, 119735, 78726, 93698,
-    148680, 144638, 83341, 149571, 147196, 54526, 91775, 63153, 143441,
-    71134, 114131, 120931, 109833, 106073, 64547, 126938, 52877, 89945,
-    59466, 79660, 147815, 55381, 100052, 78824, 121844, 104155, 117313,
-    69305, 144645, 81350, 123512, 81467, 120836, 118612, 143999, 90792,
-    71054, 138942, 56481, 71850, 85266, 77437, 86530, 147311, 133699,
-    126684, 58708, 149482, 104101, 67985, 81648, 95290, 77155, 76578,
-    116025, 83980, 59517, 62078, 89003, 126205, 122542, 116388, 144040,
-    102560, 77098, 127534, 56415, 85703, 85580, 86787, 72029, 82533,
-    132187, 70849, 98839];
+    let masses = [
+        74364, 146203, 128470, 91616, 115655, 134147, 53470, 126471, 70040, 88750, 142353, 143329,
+        86356, 118399, 97959, 148345, 117705, 87624, 63862, 71962, 106974, 66255, 119735, 78726,
+        93698, 148680, 144638, 83341, 149571, 147196, 54526, 91775, 63153, 143441, 71134, 114131,
+        120931, 109833, 106073, 64547, 126938, 52877, 89945, 59466, 79660, 147815, 55381, 100052,
+        78824, 121844, 104155, 117313, 69305, 144645, 81350, 123512, 81467, 120836, 118612, 143999,
+        90792, 71054, 138942, 56481, 71850, 85266, 77437, 86530, 147311, 133699, 126684, 58708,
+        149482, 104101, 67985, 81648, 95290, 77155, 76578, 116025, 83980, 59517, 62078, 89003,
+        126205, 122542, 116388, 144040, 102560, 77098, 127534, 56415, 85703, 85580, 86787, 72029,
+        82533, 132187, 70849, 98839,
+    ];
 
     // PART 1
     //m is a borrow, needs dereferencing! (*m)
@@ -20,7 +19,10 @@ fn main() {
     println!("Part 1 answer: {}", sum);
 
     // PART 2
-    let sum: u32 = masses.iter().map(|m| compute_fuel_and_fuel_for_fuel(*m, 0)).sum();
+    let sum: u32 = masses
+        .iter()
+        .map(|m| compute_fuel_and_fuel_for_fuel(*m, 0))
+        .sum();
 
     //Use the example to make sure we didn't mess up?
     assert_eq!(compute_fuel_and_fuel_for_fuel(1969, 0), 966);
@@ -28,24 +30,22 @@ fn main() {
     println!("Part 2 answer: {}", sum);
 }
 
-fn compute_fuel(mass: u32) -> u32
-{
+fn compute_fuel(mass: u32) -> u32 {
     /* Fuel required to launch a given module is based on its mass.
      * Specifically, to find the fuel required for a module, take
      * its mass, divide by three, round down, and subtract 2.
      */
 
     //Sweet, Rust rounds down on integer division
-    mass/3 - 2
+    mass / 3 - 2
 }
 
-fn compute_fuel_and_fuel_for_fuel(mass: u32, mut total_fuel: u32) -> u32
-{
+fn compute_fuel_and_fuel_for_fuel(mass: u32, mut total_fuel: u32) -> u32 {
     /* Fuel itself requires fuel just like a module - take its mass,
      * divide by three, round down, and subtract 2. However, that fuel
      * also requires fuel, and that fuel requires fuel, and so on.
      */
-    
+
     //How much fuel does this mass require?
     let fuel_mass = compute_fuel(mass);
 
@@ -55,9 +55,8 @@ fn compute_fuel_and_fuel_for_fuel(mass: u32, mut total_fuel: u32) -> u32
     let mut result = total_fuel;
     //"Any mass that would require negative fuel should instead be treated as if it requires zero fuel"
     // (This is our recursion end condition :3)
-    if fuel_mass >= 9 
-    {
-        //If the required fuel is heavy enough, it requires its own additional amount of fuel. 
+    if fuel_mass >= 9 {
+        //If the required fuel is heavy enough, it requires its own additional amount of fuel.
         result = compute_fuel_and_fuel_for_fuel(fuel_mass, total_fuel);
     }
 
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,24 +1,29 @@
 //Defines what Intcode instructions look like~
-enum IntcodeInstruction
-{
-    Add {lhs: u32, rhs: u32, dest: u32},
-    Mul {lhs: u32, rhs: u32, dest: u32},
-    Halt
+enum IntcodeInstruction {
+    Add { lhs: u32, rhs: u32, dest: u32 },
+    Mul { lhs: u32, rhs: u32, dest: u32 },
+    Halt,
 }
-impl IntcodeInstruction
-{
+impl IntcodeInstruction {
     //Instructions are built from a slice (of which 1 element is read, and perhaps 3 more)
     // We also keep track of the index in the program, and hope we'll only need to move forward through those instructions ^u^'
-    fn new(params: &[u32], index: &mut usize) -> Self
-    {
+    fn new(params: &[u32], index: &mut usize) -> Self {
         match params[0] {
             1 => {
                 *index += 4;
-                IntcodeInstruction::Add {lhs: params[1], rhs: params[2], dest: params[3]}
-            },
+                IntcodeInstruction::Add {
+                    lhs: params[1],
+                    rhs: params[2],
+                    dest: params[3],
+                }
+            }
             2 => {
                 *index += 4;
-                IntcodeInstruction::Mul {lhs: params[1], rhs: params[2], dest: params[3]}
+                IntcodeInstruction::Mul {
+                    lhs: params[1],
+                    rhs: params[2],
+                    dest: params[3],
+                }
             }
             99 => IntcodeInstruction::Halt,
             _ => {
@@ -29,16 +34,15 @@ impl IntcodeInstruction
 }
 
 //Executing an instruction means modifying the intcode program, so keep a mutable reference to it!
-fn execute_at(index: &mut usize, intcode: &mut [u32])
-{
+fn execute_at(index: &mut usize, intcode: &mut [u32]) {
     let instruction = IntcodeInstruction::new(&intcode[*index..], index);
     match instruction {
-        IntcodeInstruction::Add {lhs, rhs, dest} => {
+        IntcodeInstruction::Add { lhs, rhs, dest } => {
             intcode[dest as usize] = intcode[lhs as usize] + intcode[rhs as usize];
-        },
-        IntcodeInstruction::Mul {lhs, rhs, dest} => {
+        }
+        IntcodeInstruction::Mul { lhs, rhs, dest } => {
             intcode[dest as usize] = intcode[lhs as usize] * intcode[rhs as usize];
-        },
+        }
         IntcodeInstruction::Halt => {
             //That's one way of making sure the program halts ¯\_(ツ)_/¯ (with the appropriate condition in execute)
             *index = intcode.len()
@@ -47,11 +51,11 @@ fn execute_at(index: &mut usize, intcode: &mut [u32])
 }
 
 //Repeatedly execute the instruction at the current index.
-fn execute(mut intcode: &mut [u32])
-{
+fn execute(mut intcode: &mut [u32]) {
     let mut index: usize = 0;
-    while index < intcode.len() //Don't run further than expected/stop on Halt
-    {
+
+    //Don't run further than expected, and stop on Halt
+    while index < intcode.len() {
         //Index will change as soon as we create a new IntcodeInstruction.
         execute_at(&mut index, &mut intcode)
     }
@@ -60,7 +64,13 @@ fn execute(mut intcode: &mut [u32])
 fn main() {
     // PART 1
     // INPUT
-    let mut intcode = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,6,19,23,2,23,6,27,2,6,27,31,2,13,31,35,1,10,35,39,2,39,13,43,1,43,13,47,1,6,47,51,1,10,51,55,2,55,6,59,1,5,59,63,2,9,63,67,1,6,67,71,2,9,71,75,1,6,75,79,2,79,13,83,1,83,10,87,1,13,87,91,1,91,10,95,2,9,95,99,1,5,99,103,2,10,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0];
+    let mut intcode = [
+        1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 1, 6, 19, 23, 2, 23, 6, 27,
+        2, 6, 27, 31, 2, 13, 31, 35, 1, 10, 35, 39, 2, 39, 13, 43, 1, 43, 13, 47, 1, 6, 47, 51, 1,
+        10, 51, 55, 2, 55, 6, 59, 1, 5, 59, 63, 2, 9, 63, 67, 1, 6, 67, 71, 2, 9, 71, 75, 1, 6, 75,
+        79, 2, 79, 13, 83, 1, 83, 10, 87, 1, 13, 87, 91, 1, 91, 10, 95, 2, 9, 95, 99, 1, 5, 99,
+        103, 2, 10, 103, 107, 1, 107, 2, 111, 1, 111, 5, 0, 99, 2, 14, 0, 0,
+    ];
     //Initialize the thing
     intcode[1] = 12;
     intcode[2] = 2;
@@ -75,34 +85,62 @@ fn main() {
 mod tests {
     #[test]
     fn part1_ex1() {
-        let mut intcode = [1,0,0,0,99];
+        let mut intcode = [1, 0, 0, 0, 99];
         crate::execute(&mut intcode);
         //Oof.
         // - Get an iterator over that array,
         // - map() each u32 to a String,
         // - collect the values into a Vec (it cannot be a slice, since those are borrows!)
         // - finally, join the pieces together.
-        assert_eq!(intcode.into_iter().map(|i:&u32| i.to_string()).collect::<Vec<String> >().join(","), "2,0,0,0,99");
+        assert_eq!(
+            intcode
+                .into_iter()
+                .map(|i: &u32| i.to_string())
+                .collect::<Vec<String>>()
+                .join(","),
+            "2,0,0,0,99"
+        );
     }
 
     #[test]
     fn part1_ex2() {
-        let mut intcode = [2,3,0,3,99];
+        let mut intcode = [2, 3, 0, 3, 99];
         crate::execute(&mut intcode);
-        assert_eq!(intcode.into_iter().map(|i:&u32| i.to_string()).collect::<Vec<String> >().join(","), "2,3,0,6,99");
+        assert_eq!(
+            intcode
+                .into_iter()
+                .map(|i: &u32| i.to_string())
+                .collect::<Vec<String>>()
+                .join(","),
+            "2,3,0,6,99"
+        );
     }
 
     #[test]
     fn part1_ex3() {
-        let mut intcode = [2,4,4,5,99,0];
+        let mut intcode = [2, 4, 4, 5, 99, 0];
         crate::execute(&mut intcode);
-        assert_eq!(intcode.into_iter().map(|i:&u32| i.to_string()).collect::<Vec<String> >().join(","), "2,4,4,5,99,9801");
+        assert_eq!(
+            intcode
+                .into_iter()
+                .map(|i: &u32| i.to_string())
+                .collect::<Vec<String>>()
+                .join(","),
+            "2,4,4,5,99,9801"
+        );
     }
 
     #[test]
     fn part1_ex4() {
-        let mut intcode = [1,1,1,4,99,5,6,0,99];
+        let mut intcode = [1, 1, 1, 4, 99, 5, 6, 0, 99];
         crate::execute(&mut intcode);
-        assert_eq!(intcode.into_iter().map(|i:&u32| i.to_string()).collect::<Vec<String> >().join(","), "30,1,1,4,2,5,6,0,99");
+        assert_eq!(
+            intcode
+                .into_iter()
+                .map(|i: &u32| i.to_string())
+                .collect::<Vec<String>>()
+                .join(","),
+            "30,1,1,4,2,5,6,0,99"
+        );
     }
 }
